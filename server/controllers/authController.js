b80365b653052a1e4187b6811c80ee824c54179f
Andiswa Molangathi <andiswacyriam@gmail.com>
2025-06-30 17:43:36 +0200

    Create authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
+  });
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (await User.findOne({ email })) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({ name, email, password });
+    const token = generateToken(user);
+
+    res.status(201).json({
+      user: { id: user._id, name: user.name, email: user.email },
+      token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = generateToken(user);
+
+    res.json({
+      user: { id: user._id, name: user.name, email: user.email },
+      token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
